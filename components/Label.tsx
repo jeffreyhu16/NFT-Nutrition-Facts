7e64330b9afaabc4347a0d0d12221d0130cc88dd
Jeffrey <jeffreyhu16@gmail.com>
2022-10-24 14:25:01 +0800

    add Label component

---
+++ components/Label.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Label.module.css'
+
+const Label = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.header}>NFT Nutrition Label</h1>
+    </div>
+  );
+}
+ 
+export default Label;
