fa05f6d762378ef7357081000403e8db512fe435
Jeffrey <jeffreyhu16@gmail.com>
2022-10-24 14:29:03 +0800

    add fonts

--- components/Label.tsx
+++ components/Label.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/Label.module.css'
 const Label = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.header}>NFT Nutrition Label</h1>
+      <h1 className={styles.header}>NFT Nutrition</h1>
     </div>
   );
 }
