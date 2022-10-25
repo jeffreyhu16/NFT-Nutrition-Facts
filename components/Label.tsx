36bcfe8ea589076ee3378365f328b63d7826aa00
Jeffrey <jeffreyhu16@gmail.com>
2022-10-25 10:21:24 +0800

    add label input

--- components/Label.tsx
+++ components/Label.tsx
@@ -1,11 +1,30 @@
+import { ChangeEvent, useState } from 'react';
 import styles from '../styles/Label.module.css'
 
 const Label = () => {
+
+  const [input, setInput] = useState<string>();
+
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  }
+
   return (
     <div className={styles.container}>
-      <h1 className={styles.header}>NFT Nutrition</h1>
+      <form className={styles.form}>
+        <input
+          type='text'
+          className={styles.input}
+          placeholder='Contract Address'
+          value={input}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => changeHandler(e)}
+        />
+      </form>
+      <div className={styles.label}>
+        <h1 className={styles.header}>NFT Nutrition</h1>
+      </div>
     </div>
   );
 }
- 
+
 export default Label;
