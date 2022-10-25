fafa687f8b0a950b4895dbd45d6f9a0d38e25916
Jeffrey <jeffreyhu16@gmail.com>
2022-10-25 13:39:48 +0800

    add verify contract function

--- components/Label.tsx
+++ components/Label.tsx
@@ -1,17 +1,40 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react'
 import styles from '../styles/Label.module.css'
 
 const Label = () => {
 
-  const [input, setInput] = useState<string>();
+  const [input, setInput] = useState<string>('');
 
   const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
   }
 
+  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const isVerified = await verifyContract();
+    console.log(isVerified)
+  }
+
+  const verifyContract = async () => {
+    const res = await fetch(
+      'https://api.etherscan.io/api' +
+      '?module=contract' +
+      '&action=getsourcecode' +
+      `&address=${input}` +
+      `&apikey=${process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY}`
+    );
+    const data = await res.json();
+
+    return data.status === '1' ? true : false;
+  }
+
   return (
     <div className={styles.container}>
-      <form className={styles.form}>
+      <form
+        className={styles.form}
+        onSubmit={(e: FormEvent<HTMLFormElement>) => submitHandler(e)}
+      >
         <input
           type='text'
           className={styles.input}
